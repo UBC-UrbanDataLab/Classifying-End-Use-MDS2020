bd1c1d0b4546602e249655e5e62ed26ae01ed51e
Alex T <myaddress@realemail.net>
2020-05-27 14:00:30 -0700

    Fixed docstring, moved test code to sep unit test

--- code/up_only.py
+++ code/up_only.py
@@ -6,30 +6,24 @@ import numpy as np
 
 
 
-'''
-agg_update_rate
-arguments:
-    dataframe = pandas dataframe with total number of updates each day for each sensor over some date range
-returns:
-    A dataframe with sensor id tags and columns for average number of updates per day, variance in number of updates per day
 
-'''
 
 def agg_up_rate(dataframe):
+    """Function for calculating aggregated daily sensor update rates
+
+    Args:
+        dataframe (pandas.dataFrame): dataframe containing sensor id tags, date, and number of updates for the day
+        
+    Returns
+        Pandas.dataFrame: dataframe with sensor id tags and columns for mean number of updates per day, variance in number of updates per day, and max number of updates per day
+
+    """
+
     try:
         grouped = dataframe.groupby(["equipRef","groupRef","navName","typeRef","unit"])
         num_updates_df = grouped.agg([np.mean,np.median,np.std,np.max]) #no reason to include min. Will be 0 due to server outage days
         num_updates_df.reset_index(inplace=True)
         return(num_updates_df)
     except:
-        print("Error in agg_up_rate()")
+        #print("Error in agg_up_rate()")
         return(0)
-
-# Test the function
-
-test_df = pd.read_csv("test_data/numSensorUpdatesApr01ToMay20.csv")
-
-results = agg_up_rate(test_df)
-
-print(results.head(20))
-
