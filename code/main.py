c8cf79a117caaea67eb3d5cf30937c2f490f8293
Alex T <myaddress@realemail.net>
2020-05-28 15:02:25 -0700

    Original state from connor

---
+++ code/main.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 27 16:27:50 2020
+
+@author: connor
+"""
+
+def main():
+    # Query EC data
+    
+    # Query NC data
+    
+    # Query weather data (join to EC and NC data)
+    
+    # Read in SkySpark metadata
+    
+    # Join EC data with SkySpark metadata
+    
+    # Join NC data with SkySpark metadata
+    
+    # Aggregate "values" and calculate sensor update rate
+    
+    # Identify any groupings in the sensor tags/names
+    
+    # Categorical Feature selection
+    
+    # Continuous Feature selection
+    
+    # Drop unwanted categorical and continuous features
+    
+    # Scale "values" and encode categorical features
+    
+    #################################
+    # Cluster NC sensors
+    
+    # Join NC and EC Datasets
+    
+    #################################
+    
+    # Seperate labeled/unlabeled data
+    
+    # Train model/load (do we need to train the model every time, could we just save and load it?)
+    
+    # Predict NRCan Labels
