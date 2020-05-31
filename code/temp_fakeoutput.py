3e979aea12dae4a455a4ec977eda660b83a19462
Alex T <myaddress@realemail.net>
2020-05-30 19:27:21 -0700

    fakeoutput ready for pivot

--- code/temp_fakeoutput.py
+++ code/temp_fakeoutput.py
@@ -9,10 +9,14 @@ fake_step1_output["clust_group_num"] = np.random.randint(1, 5, fake_step1_output
 fake_step1_output = aggregation.split_datetime(fake_step1_output)
 fake_step1_output.drop(["equipRef","groupRef","navName","siteRef","typeRef","unit", "month"], axis=1, inplace=True)
 
-fake_step1_output = aggregation.agg_all(fake_step1_output, num_how="mean", col_idx=[2,4,3],last_idx_to_col=False)
+fake_step1_output = aggregation.agg_all(fake_step1_output, num_how="mean", col_idx=[3,4,2],last_idx_to_col=False)
 fake_step1_output = fake_step1_output.assign(stddev_value=np.random.uniform(1, 5, fake_step1_output.shape[0]),
 min_value=np.random.uniform(1, 5, fake_step1_output.shape[0]),
 max_value=np.random.uniform(1, 5, fake_step1_output.shape[0]),
 mean_update_rate=np.random.uniform(1, 5, fake_step1_output.shape[0]))
 fake_step1_output.rename(columns={"value":"mean_value"}, inplace=True)
+#Need to pivot table this to get in the format needed!
+#
+#
 print(fake_step1_output.head(25))
+#
