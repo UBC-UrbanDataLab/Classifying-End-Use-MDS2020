005b45f862cef1d12efb55dc3866c0a5dee8cf94
Connor Lee <42241585+connordlee@users.noreply.github.com>
2020-06-24 09:22:39 -0700

    Update code/data_preparation.py
    Co-authored-by: Alex T <48026177+10001Monkeys@users.noreply.github.com>

--- code/data_preparation.py
+++ code/data_preparation.py
@@ -509,7 +509,7 @@ def query_db_ec(client, date, num_days=1, site='Pharmacy'):
         return None
     
 def query_db_nc(client, date, num_days=1, site='Pharmacy'):
-    """Function to query the UBC_EWS database for the Non-Energy Consumption (NC) sensors
+    """Function to query the UBC_EWS measurement for the Non-Energy Consumption (NC) sensors
     for the user defined start date, number of days (default=1), and site (default=Pharmacy)
 
     Args:
